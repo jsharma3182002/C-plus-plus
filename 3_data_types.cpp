5a948ee640550ff1173e4bd3f39e8ea43e929d36
Jatin Sharma <91322311+jsharma3182002@users.noreply.github.com>
2022-01-29 14:46:08 +0530

    Basics data types

---
+++ 3_data_types.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=55,b=76;
+    float pi=3.14;
+    char c='s';
+    cout<<"The value of a is "<<a<< "\nThe value of b is "<<b<<"\nThe value of pi is "<<pi<<"\nThe purpose of the  program is to learn "<<c;
+    return 0;
+}
